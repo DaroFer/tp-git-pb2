e17f3c41c18ac4fa3bb80240d546320bb8867943
unknown <dariofer2319@gmail.com>
2020-04-20 11:12:15 -0300

    Resubido archivo original

--- Calculadora.java
+++ Calculadora.java
@@ -1,34 +1,28 @@
 import java.util.Scanner;
-public class Calculadora {
-       
-        public static void main(String[] args)  {          
-            Integer a;    
-            Integer b;          
-            Integer opcion;
-            Integer resultado=0;
-           
-            Scanner teclado = new Scanner(System.in);
-            do{
-                System.out.println("Ingrese su operación \n 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir");  
-            opcion = teclado.nextInt();       
-            }
-            while (opcion < 1 && opcion > 4);
-            System.out.println("Ingrese el primer numero");       
-            a = teclado.nextInt();
-           
-            System.out.println("Ingrese el segundo numero");  
-            b = teclado.nextInt(); 
-            switch (opcion)
-            {   case 1:resultado = a + b;
-                break;   
-                case 2:resultado = a - b;
-                break;
-                case 3: resultado = a * b;
-                break;      
-                case 4: resultado = a / b;
-                break;
-                          
-            }
-            System.out.println("El resultado es " + resultado.toString());
-            }
-        }
+public class Ahorcado {
+ public static void main(String[] args) {
+ Integer a;
+ Integer b;
+ Integer opcion;
+ Integer resultado;
+ do{
+ System.out.println("Ingrese su operación /n 1 para sumar /n 2 para
+restar /n 3 para multiplicar /n 4 para dividir");
+ opcion = teclado.next();
+ }while(opcion < 1 && opcion > 4)
+ System.out.println("Ingrese el primer numero");
+ a = teclado.next();
+ System.out.println("Ingrese el segundo numero");
+ a = teclado.next();
+ if(opcion.equals(1)){
+ resultado = a + b;
+ }else if(opcion.equals(2)){
+ resultado = a - b;
+ }else if(opcion.equals(3)){
+ resultado = a * b;
+ }else{
+ resultado = a / b;
+ }
+ System.out.println("El resultado es " + resultado.toString());
+ }
+}
