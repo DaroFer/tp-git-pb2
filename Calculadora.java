b26214fb3daea923f046ffa6fa4b8b770c9044fe
unknown <dariofer2319@gmail.com>
2020-04-20 13:05:38 -0300

    restar 4, dividir 2)

--- Calculadora.java
+++ Calculadora.java
@@ -9,7 +9,7 @@ public class Calculadora {
            
             Scanner teclado = new Scanner(System.in);
             do{
-                System.out.println("Ingrese su operación \n 3 para sumar \n 2 para restar \n 1 para multiplicar \n 4 para dividir");  
+                System.out.println("Ingrese su operación \n 3 para sumar \n 4 para restar \n 1 para multiplicar \n 2 para dividir");  
             opcion = teclado.nextInt();       
             }
             while (opcion < 1 && opcion > 4);
@@ -21,11 +21,11 @@ public class Calculadora {
             switch (opcion)
             {   case 1:resultado = a * b;
                 break;   
-                case 2:resultado = a - b;
+                case 2:resultado = a / b;
                 break;
                 case 3: resultado = a + b;
                 break;      
-                case 4: resultado = a / b;
+                case 4: resultado = a - b;
                 break;
                           
             }
