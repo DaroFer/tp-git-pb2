f002698e4cdafeb8e184295774e1c9a1390168fb
unknown <dariofer2319@gmail.com>
2020-04-20 23:46:32 -0300

    se hace revert

--- Calculadora.java
+++ Calculadora.java
@@ -9,7 +9,8 @@ public class Calculadora {
            
             Scanner teclado = new Scanner(System.in);
             do{
-                System.out.println("Ingrese su operación \n 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir");  
+
+                System.out.println("Ingrese su operación \n 3 para sumar \n 2 para restar \n 1 para multiplicar \n 4 para dividir");  
             opcion = teclado.nextInt();       
             }
             while (opcion < 1 && opcion > 4);
@@ -19,12 +20,11 @@ public class Calculadora {
             System.out.println("Ingrese el segundo numero");  
             b = teclado.nextInt(); 
             switch (opcion)
-
-            {   case 1:resultado = a + b;
+            {   case 1:resultado = a * b;
                 break;   
                 case 2:resultado = a - b;
                 break;
-                case 3: resultado = a * b;
+                case 3: resultado = a + b;
                 break;      
                 case 4: resultado = a / b;
                 break;
